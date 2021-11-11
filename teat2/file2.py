fa77594595817e385538e2d3628ee2db440b267b
Arina <92784229+ArinaYakovenko@users.noreply.github.com>
2021-11-11 14:20:17 +0200

    Create file2.py

---
+++ teat2/file2.py
@@ -0,0 +1,24 @@
+#test number 1
+a=int(input())
+b=int(input())
+c=int(input())
+d=[]
+if a>0:
+    d.append(a)
+if b>0:
+    d.append(b)
+if c>0:
+    d.append(c)
+print(d)
+
+#test number 2
+for i in range(1, 497+1):
+    if i%2==0:
+        print(i)
+      
+#test number 3
+summa=0
+for i in range(0, 14+1):
+    summa+=i
+print(summa)
+
