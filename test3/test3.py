aa5bf900fb3ae6064b3ebb0b2bdb980566271995
Arina <92784229+ArinaYakovenko@users.noreply.github.com>
2021-11-15 12:27:04 +0200

    Create test3.py

---
+++ test3/test3.py
@@ -0,0 +1,18 @@
+#test number 1
+list=['1','2','3']
+print(list[::-1])
+
+#test number 2
+lst=['1','2','3']
+lst[0],lst[-1]=lst[-1],lst[0]
+print(lst)
+
+#test number 3
+to_list=input()
+list=[]
+list.append(to_list)
+print(list)
+
+#test number 4
+list=[1, 23, 5, 10]
+print(max(list)/len(list))
